07902deef2593a47588339ef5003c4afc650a97b
abdul <abdul.tanveer@gmail.com>
2022-05-16 10:05:24 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/tataauto/MainActivity.java
@@ -0,0 +1,23 @@
+package com.abdul.tataauto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handleClick(View view) {
+
+        Toast.makeText(this, "welcome to automotive", Toast.LENGTH_SHORT).show();
+    }
+}
