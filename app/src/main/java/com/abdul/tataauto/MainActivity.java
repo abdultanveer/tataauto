cafac586641e65d4d09b46efc00b3f04d091db63
abdul <abdul.tanveer@gmail.com>
2022-05-17 10:29:28 +0530

    starting another activity

--- app/src/main/java/com/abdul/tataauto/MainActivity.java
+++ app/src/main/java/com/abdul/tataauto/MainActivity.java
@@ -2,6 +2,7 @@ package com.abdul.tataauto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,9 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);//inflation
 
         nameEditText = findViewById(R.id.etName); //initialization
+
     }
 
     public void handleClick(View view) {
+        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(hIntent);
+
         String name = nameEditText.getText().toString();
 
         Toast.makeText(this, name + "----welcome to automotive", Toast.LENGTH_SHORT).show();
