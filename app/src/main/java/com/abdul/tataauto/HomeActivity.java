d5b01c612077143c439a967e8e459a8141cc63a6
abdul <abdul.tanveer@gmail.com>
2022-05-17 14:09:22 +0530

    activity lifecycle -- logcat

--- app/src/main/java/com/abdul/tataauto/HomeActivity.java
+++ app/src/main/java/com/abdul/tataauto/HomeActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-EditText contactEditText;
+    EditText contactEditText;
+    public static String TAG = HomeActivity.class.getSimpleName();
+            //"HomeActivity"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        Log.i(TAG,"onCreate");
         contactEditText = findViewById(R.id.etContact);
 
         Intent receivedIntentMA = getIntent();
@@ -22,6 +25,37 @@ EditText contactEditText;
         String mName = extrasBox.getString("labelkey");
         TextView homeTextView = findViewById(R.id.tvHome); //getting handle - hold of tvHome
         homeTextView.setText(mName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onstart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"onresume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG,"onpause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,"onstop");
+    }
+
+    //purged
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG,"ondestroy");
     }
 
     /**
